49343a28b11ce368debdb1889e90faaa22bc2fb0
Bill Lieu <bslieu19@gmail.com>
2022-06-08 21:17:36 -0700

    Testing Ansible with Terraform local-exec

--- main.tf
+++ main.tf
@@ -63,3 +63,7 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+
+########################### Terraform-Ansible ###############################
+
+provisioner "local-exec" {command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u {var.user} -i '${self.ipv4_address},' --private-key ${var.ssh_private_key} playbook.yml"}
