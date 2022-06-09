ff0ad859bcac493c666f0617303b0bf9bcf54cb5
Bill Lieu <bslieu19@gmail.com>
2022-06-09 07:07:06 -0700

    Investigate the directory code is running in

--- main.tf
+++ main.tf
@@ -75,7 +75,8 @@ resource "aws_instance" "web" {
   }
   ########################### Terraform-Ansible ###############################
   provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u {var.user} -i ${self.public_ip} --private-key ${tls_private_key.aws_keys.private_key_pem} ${github.workspace}/playbook.yml"
+    command = pwd
+    #"ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u {var.user} -i ${self.public_ip} --private-key ${tls_private_key.aws_keys.private_key_pem} ${github.workspace}/playbook.yml"
   }
 }
 
